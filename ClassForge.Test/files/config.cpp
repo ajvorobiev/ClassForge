5a1f2d2bfcd229a4f4eecf2a7999d7ee2fb71bd5
Alex Vorobiev <vorobiev.a.j@gmail.com>
2015-05-06 02:19:48 +0200

    Better test file

--- ClassForge.Test/files/config.cpp
+++ ClassForge.Test/files/config.cpp
@@ -1,11 +1,95 @@
-class class1
+class rhs_acc_1pn93_2 : rhs_acc_pgo7v
 {
-    var1class1 = 3;
-    var2class1  = "sometext";
-    var3class1 []= {"somearray1","somearray2" };
+	Author_Macro
+	scope = 2;
+
+	displayName = $STR_RHS_ACC_1PN932;
+
+	picture = "\rhsafrf\addons\rhs_weapons\gear_acc\rhs_acc_gear_1pn93_ca.paa";
+
+	model = "\rhsafrf\addons\rhs_weapons\acc\scopes\1pn93\rhs_1pn93_2";
+
+	descriptionShort = $STR_RHS_ACC_1PN932_SD;
+
+	class ItemInfo : InventoryOpticsItem_Base_F
+	{
+		opticType = 2;
+		mass = 13.64;
+		RMBhint = "Optical Sight";
+		optics = 1;
+
+
+		modelOptics = "\rhsafrf\addons\rhs_weapons\acc\scopes\optics\rhs_1pn932_sniper_scope";
+
+		class OpticsModes
+		{
+			///! 2.7X MAGNIFICATION -> 0.2492/2.7
+			class rhs_pgo7v_scope
+			{
+				opticsID = 2;
+				useModelOptics = 1;
+				opticsPPEffects[] = { "OpticsCHAbera1", "OpticsBlur1" };
+				discreteDistance[] = { 500 };
+				discreteDistanceInitIndex = 0;
+				opticsZoomMin = 0.35 / 2.7;
+				opticsZoomMax = 0.35 / 2.7;
+				opticsZoomInit = 0.35 / 2.7;
+				memoryPointCamera = "opticView";
+				visionMode[] = { "NVG" };
+				opticsFlare = 1;
+				opticsDisablePeripherialVision = 1;
+				distanceZoomMin = 200;
+				distanceZoomMax = 1000;
+				cameraDir = "direction";
+			};
+		};
+	};
 };
 
-class class2 : class1
+class rhs_acc_1pn93_2 : rhs_acc_pgo7v
 {
-    var1class2 = 3.24;
+	Author_Macro
+	scope = 2;
+
+	displayName = $STR_RHS_ACC_1PN932;
+
+	picture = "\rhsafrf\addons\rhs_weapons\gear_acc\rhs_acc_gear_1pn93_ca.paa";
+
+	model = "\rhsafrf\addons\rhs_weapons\acc\scopes\1pn93\rhs_1pn93_2";
+
+	descriptionShort = $STR_RHS_ACC_1PN932_SD;
+
+	class ItemInfo : InventoryOpticsItem_Base_F
+	{
+		opticType = 2;
+		mass = 13.64;
+		RMBhint = "Optical Sight";
+		optics = 1;
+
+
+		modelOptics = "\rhsafrf\addons\rhs_weapons\acc\scopes\optics\rhs_1pn932_sniper_scope";
+
+		class OpticsModes
+		{
+			///! 2.7X MAGNIFICATION -> 0.2492/2.7
+			class rhs_pgo7v_scope
+			{
+				opticsID = 2;
+				useModelOptics = 1;
+				opticsPPEffects[] = { "OpticsCHAbera1", "OpticsBlur1" };
+				discreteDistance[] = { 500 };
+				discreteDistanceInitIndex = 0;
+				opticsZoomMin = 0.35 / 2.7;
+				opticsZoomMax = 0.35 / 2.7;
+				opticsZoomInit = 0.35 / 2.7;
+				memoryPointCamera = "opticView";
+				visionMode[] = { "NVG" };
+				opticsFlare = 1;
+				opticsDisablePeripherialVision = 1;
+				distanceZoomMin = 200;
+				distanceZoomMax = 1000;
+				cameraDir = "direction";
+			};
+		};
+	};
 };
