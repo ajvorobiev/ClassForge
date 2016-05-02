b1e5dd6b350c8c9ac62125f80780a3e3e6f00ed3
Alex Vorobiev <vorobiev.a.j@gmail.com>
2016-05-02 18:02:25 +0200

    Bumped to 1.14, more Json stuff.

---
+++ ClassForge/Model/DereferencedHeader.cs
@@ -0,0 +1,38 @@
+﻿namespace ClassForge.Model
+{
+    /// <summary>
+    /// Header for the dereferenced model output.
+    /// </summary>
+    public class DereferencedHeader
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version.
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the top level filters.
+        /// </summary>
+        public string TopLevelFilters { get; set; }
+
+        /// <summary>
+        /// Gets or sets the bottom level filters.
+        /// </summary>
+        public string BottomLevelFilters { get; set; }
+
+        /// <summary>
+        /// Gets or sets the property filters.
+        /// </summary>
+        public string PropertyFilters { get; set; }
+    }
+}
---
+++ ClassForge/Model/DereferencedStructure.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassForge.Model
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Holds the full dereferenced model necessary for export.
+    /// </summary>
+    public class DereferencedStructure
+    {
+        /// <summary>
+        /// Deserializes the json into this object.
+        /// </summary>
+        /// <param name="json">The json to deserialize.</param>
+        /// <returns>An instance of <see cref="DereferencedStructure"/> containing the deserialized information.</returns>
+        public static DereferencedStructure FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<DereferencedStructure>(json);
+        }
+
+        /// <summary>
+        /// Gets or sets the header.
+        /// </summary>
+        public DereferencedHeader Header { get; set; }
+
+        /// <summary>
+        /// Gets or sets the model.
+        /// </summary>
+        public DereferencedModel Model { get; set; }
+    }
+}
